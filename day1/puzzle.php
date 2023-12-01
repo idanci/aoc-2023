032a277d448a9f538bf5d2a0e472eb8bf7586402
Ivan Danci <danci.ivan@gmail.com>
2023-12-01 13:24:27 +0100

    chore: first day

---
+++ day1/puzzle.php
@@ -0,0 +1,41 @@
+<?php
+  $inputs = array("example.txt", "input.txt");
+
+  foreach($inputs as $input) {
+    $myfile = fopen($input, "r") or die("Unable to open file!");
+    $sum = 0;
+
+    while(!feof($myfile)) {
+      $line = fgets($myfile);
+
+      $chars = str_split($line);
+
+      $first_digit = null;
+      $last_digit = null;
+
+      foreach($chars as $char){
+        $int_char = intval($char);
+
+        $is_letter = $int_char == 0 && $char != '0';
+
+        if($is_letter) {
+          continue;
+        }
+
+        if($first_digit == null) {
+          $first_digit = $int_char;
+          $last_digit = $int_char;
+
+          continue;
+        }
+
+        $last_digit = $int_char;
+      }
+      $sum += intval($first_digit . $last_digit);
+    }
+
+    echo $input . ": " . $sum . "\n";
+
+    fclose($myfile);
+  }
+?>

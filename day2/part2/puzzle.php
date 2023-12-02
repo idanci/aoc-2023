bc063b035f746dd6590e865c660caf4254f010f3
Ivan Danci <danci.ivan@gmail.com>
2023-12-02 17:04:56 +0100

    solve day2 part2

--- day2/part2/puzzle.php
+++ day2/part2/puzzle.php
@@ -1,7 +1,37 @@
 <?php
-  const BALLS = array('red' => 12, 'green' => 13, 'blue' => 14);
-
   $inputs = array("example.txt", "input.txt");
+
+  function calculate_product($line) {
+    $all_games_line = explode(":", $line)[1];
+    $all_games = explode(";", $all_games_line);
+
+    $max_by_color = array('red' => 0, 'green' => 0, 'blue' => 0);
+
+    foreach($all_games as $game_line) {
+      $colors = explode(",", $game_line);
+
+      foreach($colors as $color) {
+        $color_parts = explode(" ", $color);
+
+        $color = trim($color_parts[count($color_parts) - 1]);
+        $count = intval($color_parts[1]);
+
+        if($max_by_color[$color] < $count) {
+          $max_by_color[$color] = $count;
+        }
+      }
+    }
+
+    $product = 1;
+
+    foreach($max_by_color as $max_color) {
+      if($max_color > 0){
+        $product *= $max_color;
+      }
+    }
+
+    return $product;
+  }
 
   foreach($inputs as $input) {
     $myfile = fopen($input, "r") or die("Unable to open file!");
@@ -11,7 +41,10 @@
     while(!feof($myfile)) {
       $line = fgets($myfile);
 
-      // TODO
+      if($line){
+        $product_of_balls = calculate_product($line);
+        $sum += $product_of_balls;
+      }
     }
 
     echo $input . ": " . $sum . "\n";

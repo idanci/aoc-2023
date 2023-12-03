f2d844e2e63e476b21f7827ff24367e358481baf
Ivan Danci <danci.ivan@gmail.com>
2023-12-03 15:16:58 +0100

    chore: change iterator

--- day3/part1/puzzle.php
+++ day3/part1/puzzle.php
@@ -1,18 +1,29 @@
 <?php
-  $inputs = array("example.txt", "input.txt");
+  $inputs = array("example.txt");
+  // $inputs = array("example.txt", "input.txt");
 
   foreach($inputs as $input) {
     $myfile = fopen($input, "r") or die("Unable to open file!");
+    $matrix = array();
+    $line_num = 0;
 
-    while(!feof($myfile)) {
-      $line = fgets($myfile);
+    $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
 
-      if($line){
-        //
+    foreach($lines as $line){
+      $chars = str_split($line);
+      $matrix[$line_num] = array();
+      $index = 0;
+
+      foreach($chars as $char){
+        // echo '-' . $char . '-';
+        $matrix[$line_num][$index] = $char;
+        $index++;
       }
-    }
 
-    echo $input . ": " . $sum . "\n";
+      echo print_r($matrix) . "\n";
+
+      $line_num++;
+    }
 
     fclose($myfile);
   }

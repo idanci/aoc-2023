0e18306c7c8093d7ea1573c78004da771d476089
Ivan Danci <danci.ivan@gmail.com>
2023-12-03 23:42:21 +0100

    wip

--- day3/part2/puzzle.php
+++ day3/part2/puzzle.php
@@ -1,27 +1,86 @@
 <?php
-  class Gear {
-    public $x, $y;
+  class Part {
+    public $value;
+    public $x1, $x2, $row;
 
-    function __construct($x, $y) {
-      $this->x = $x;
-      $this->y = $y;
+    function __construct($value, $row, $x1, $x2) {
+      $this->value = $value;
+      $this->row = $row;
+      $this->x1 = $x1;
+      $this->x2 = $x2;
+    }
+  }
+
+  class Gear {
+    public $row, $column;
+    private $adjacent_columns;
+
+    function __construct($row, $column) {
+      $this->row = $row;
+      $this->column = $column;
+
+      $this->adjacent_columns = array($this->column - 1, $this->column, $this->column + 1);
     }
 
     function gear_ratio($parts) {
-      //
+      $adjacent_parts = array();
+
+      if(array_key_exists($this->row - 1, $parts)) {
+        foreach($parts[$this->row - 1] as $part) {
+          foreach($this->adjacent_columns as $col) {
+            if (in_array($col, range($part->x1, $part->x2))) {
+              if(in_array($part, $adjacent_parts)){
+                continue;
+              }
+              array_push($adjacent_parts, $part);
+            }
+          }
+        }
+      }
+
+      if(array_key_exists($this->row, $parts)) {
+        foreach($parts[$this->row] as $part) {
+          foreach($this->adjacent_columns as $col) {
+            if (in_array($col, range($part->x1, $part->x2))) {
+              if(in_array($part, $adjacent_parts)){
+                continue;
+              }
+              array_push($adjacent_parts, $part);
+            }
+          }
+        }
+      }
+
+      if(array_key_exists($this->row + 1, $parts)) {
+        foreach($parts[$this->row + 1] as $part) {
+          foreach($this->adjacent_columns as $col) {
+            if (in_array($col, range($part->x1, $part->x2))) {
+              if(in_array($part, $adjacent_parts)){
+                continue;
+              }
+              array_push($adjacent_parts, $part);
+            }
+          }
+        }
+      }
+
+      if(count($adjacent_parts) === 2){
+        return $adjacent_parts[0]->value * $adjacent_parts[1]->value;
+      }
+
       return 0;
     }
   }
 
-  $inputs = array("example.txt");
-  // $inputs = array("example.txt", "input.txt");
+  // $inputs = array("example.txt");
+  $inputs = array("example.txt", "input.txt");
 
   foreach($inputs as $input) {
     $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
     $height = count($lines);
 
-    $numbers = array_fill(0, $height, array());
-    $gears_matrix = array_fill(0, $height, array());
+    $parts = array_fill(0, $height, array());
+    $gears = array();
 
     $sum = 0;
 
@@ -29,19 +88,43 @@
       foreach(str_split($line) as $column => $char){
         if($char === '*'){
           $gear = new Gear($row, $column);
-          array_push($gears_matrix[$row], $gear);;
+          array_push($gears, $gear);;
         }
       }
+
+      populate_parts_from_line($row, $line, $parts);
     }
 
-    foreach($gears_matrix as $row => $gears){
-      foreach($gears as $column){
-        //
+    foreach($gears as $gear){
+      $sum += $gear->gear_ratio($parts);
+    }
+
+    echo $input . ": " . $sum . "\n";
+  }
+
+  function populate_parts_from_line($row, $line, &$parts) {
+    $numberStr = '';
+    $x1 = 0;
+    $length = strlen($line);
+
+    for ($i = 0; $i < $length; $i++) {
+      if (is_numeric($line[$i])) {
+        if ($numberStr === '') {
+          $x1 = $i; // start of a new number
+        }
+        $numberStr .= $line[$i];
+      } elseif ($numberStr !== '') {
+        $x2 = $i - 1;
+        $part = new Part(intval($numberStr), $row, $x1, $x2);
+        array_push($parts[$row], $part);
+        $numberStr = ''; // reset for the next number
       }
     }
 
-    // print_r($gears_matrix);
-
-    echo $input . ": " . $sum . "\n";
+    if ($numberStr !== '') {
+      $x2 = $length - 1;
+      $part = new Part(intval($numberStr), $row, $x1, $x2);
+      array_push($parts, $part);
+    }
   }
 ?>

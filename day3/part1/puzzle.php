948322522e1d98419477f027152b4eae212fc3bc
Ivan Danci <danci.ivan@gmail.com>
2023-12-03 18:41:09 +0100

    wip

--- day3/part1/puzzle.php
+++ day3/part1/puzzle.php
@@ -1,29 +1,51 @@
 <?php
+  class Number {
+    public $value;
+    public $x1, $x2, $row;
+
+    function __construct($value,  $row, $x1, $x2) {
+      $this->value = $value;
+      $this->row = $row;
+      $this->x1 = $x1;
+      $this->x2 = $x2;
+    }
+  }
+
   $inputs = array("example.txt");
-  // $inputs = array("example.txt", "input.txt");
 
   foreach($inputs as $input) {
     $myfile = fopen($input, "r") or die("Unable to open file!");
-    $matrix = array();
-    $line_num = 0;
-
     $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
+    $numbers = array();
+    $symbols = array_fill(0, count($lines), array());
+    $sum = 0;
 
-    foreach($lines as $line){
-      $chars = str_split($line);
-      $matrix[$line_num] = array();
-      $index = 0;
+    foreach($lines as $row => $line){
+      preg_match_all('/[0-9]+/', $line, $number_matches);
 
-      foreach($chars as $char){
-        // echo '-' . $char . '-';
-        $matrix[$line_num][$index] = $char;
-        $index++;
+      foreach($number_matches[0] as $num){
+        $x1 = strpos($line, strval($num));
+        $x2 = $x1 + strlen($num) - 1;
+
+        $number = new Number($num, $row, $x1, $x2);
+
+        array_push($numbers, $number);
       }
 
-      echo print_r($matrix) . "\n";
+      $chars = str_split($line);
 
-      $line_num++;
+      foreach($chars as $column => $char){
+        if($char === '.' || is_numeric($char)){
+          continue;
+        }
+
+        array_push($symbols[$row], $column);
+      }
     }
+
+    // print_r($numbers);
+
+    echo $input . ": " . $sum . "\n";
 
     fclose($myfile);
   }

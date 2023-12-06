79d0d304400325e361a1752e37dda4a23c3fbaa2
Ivan Danci <danci.ivan@gmail.com>
2023-12-06 22:12:42 +0100

    solve part2

--- day6/part2/puzzle.php
+++ day6/part2/puzzle.php
@@ -4,7 +4,28 @@
   foreach($inputs as $input) {
     $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
 
-    $result = null;
+    preg_match_all('/\d+/', $lines[0], $matches);
+    $time = (int)join($matches[0]);
+
+    preg_match_all('/\d+/', $lines[1], $matches);
+    $distance = (int)join($matches[0]);
+
+    $result = winnable_combinations($time, $distance);
+
     echo $input . ": " . $result . "\n";
+  }
+
+  function winnable_combinations($time, $distance) {
+    $combinations = 0;
+
+    for($ms_held = 0; $ms_held < $time; $ms_held++) {
+      $went_distance = ($time - $ms_held) * ($ms_held);
+
+      if($went_distance > $distance) {
+        $combinations++;
+      }
+    }
+
+    return $combinations;
   }
 ?>

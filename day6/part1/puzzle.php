7c79869fc5f32c688a15ec6e609cd81a6e49ed05
Ivan Danci <danci.ivan@gmail.com>
2023-12-06 22:06:40 +0100

    solve part1

--- day6/part1/puzzle.php
+++ day6/part1/puzzle.php
@@ -4,7 +4,39 @@
   foreach($inputs as $input) {
     $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
 
-    $result = null;
+    preg_match_all('/\d+/', $lines[0], $matches);
+    $times = array_map('intval', $matches[0]);
+
+    preg_match_all('/\d+/', $lines[1], $matches);
+    $distances = array_map('intval', $matches[0]);
+
+    $winnable = array();
+
+    for($i = 0; $i < count($times); $i++) {
+      $winnable[$i] = winnable_combinations($times[$i], $distances[$i]);
+    }
+
+    $result = 1;
+
+    for($i = 0; $i < count($times); $i++) {
+      if(array_key_exists($i, $winnable)) {
+        $result = $result * $winnable[$i];
+      }
+    }
     echo $input . ": " . $result . "\n";
+  }
+
+  function winnable_combinations($time, $distance) {
+    $combinations = 0;
+
+    for($ms_held = 0; $ms_held < $time; $ms_held++) {
+      $went_distance = ($time - $ms_held) * ($ms_held);
+
+      if($went_distance > $distance) {
+        $combinations++;
+      }
+    }
+
+    return $combinations;
   }
 ?>

ceab5a9c208ac07033066f5c323a77e75084dc43
Ivan Danci <danci.ivan@gmail.com>
2023-12-08 16:14:50 +0100

    wip part1

--- day8/part1/puzzle.php
+++ day8/part1/puzzle.php
@@ -1,9 +1,83 @@
 <?php
+  // $inputs = array("example.txt");
   $inputs = array("example.txt", "input.txt");
+
+  class Node {
+    public $value, $left, $right;
+
+    function __construct($value, $left, $right) {
+      $this->value = $value;
+      $this->left = $left;
+      $this->right = $right;
+    }
+  }
+
+  class Graph {
+    public $path, $nodes;
+    public $current_step, $current_node;
+    private $qwe;
+
+    function __construct($path) {
+      $this->path = $path;
+      $this->current_step = 0;
+      $this->current_node = null;
+      $this->qwe = 0;
+    }
+
+    function add_node($node) {
+      if($this->current_node === null) {
+        $this->current_node = $node;
+      }
+
+      $this->nodes[$node->value] = $node;
+    }
+
+    function walk() {
+      $direction = $this->get_direction();
+
+      if($direction === "L") {
+        $this->current_node = $this->nodes[$this->current_node->left];
+      } else {
+        $this->current_node = $this->nodes[$this->current_node->right];
+      }
+
+      $this->qwe++;
+
+      if($this->qwe > 1000) {
+        // print_r($this);
+        return;
+      }
+    }
+
+    function get_direction() {
+      $path_length = strlen($this->path);
+      $direction = $this->path[$this->current_step % $path_length];
+
+      $this->current_step++;
+
+      return $direction;
+    }
+  }
 
   foreach($inputs as $input) {
     $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
-    $result = 0;
+
+    $graph = new Graph($lines[0]);
+
+    for($i = 1; $i < count($lines); $i++) {
+      preg_match('/(?P<value>[\D]{3}).+\((?P<left>[\D]{3}).+(?P<right>[\D]{3})\)/', $lines[$i], $matches);
+
+      $node = new Node($matches['value'], $matches['left'], $matches['right']);
+
+      $graph->add_node($node);
+    }
+
+    while($graph->current_node->value !== 'ZZZ') {
+      $graph->walk();
+    }
+
+    $result = $graph->current_step + 1;
+
     echo $input . ": " . $result . "\n";
   }
 ?>

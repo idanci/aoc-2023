b9600cdb66408b21adfab54123cd717909550d50
Ivan Danci <danci.ivan@gmail.com>
2023-12-10 20:00:33 +0100

    refactor part1

--- day10/part1/puzzle.php
+++ day10/part1/puzzle.php
@@ -2,19 +2,11 @@
   $inputs = array("example.txt", "input.txt");
 
   const CONNECTIONS = array(
-    "NORTH" => ['|', 'L', 'J'],
-    "SOUTH" => ['|', '7', 'F'],
-    "WEST" => ['-', 'J', '7'],
-    "EAST" => ['-', 'L', 'F']
+    "N" => ['|', 'L', 'J'],
+    "S" => ['|', '7', 'F'],
+    "W" => ['-', 'J', '7'],
+    "E" => ['-', 'L', 'F']
   );
-
-  function get_start_coordinates(&$map) {
-    foreach($map as $row_index => $row) {
-      foreach($row as $column_index => $symbol) {
-        if($symbol === "S") return array($row_index, $column_index);
-      }
-    }
-  }
 
   foreach($inputs as $input) {
     $lines = file($input, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
@@ -29,55 +21,56 @@
     echo $input . ": " . $result . "\n";
   }
 
-  function walk($current_pipe_coordinates, &$map) {
+  function walk($start_position, &$map) {
     $path = array();
-    $path[] = $current_pipe_coordinates;
+    $path[] = $start_position;
 
-    while(true) {
+    $step_two_coords = second_tile_coordinates($start_position[0], $start_position[1], $map);
+    $path[] = $step_two_coords;
+
+    do {
       $next_pipe_coords = next_pipe_coordinates($path, $map);
-
-      if($next_pipe_coords === null) break;
-    }
+    } while($next_pipe_coords !== null);
 
     return $path;
   }
 
-  function step_two_tile_coordinates($current_row, $current_column, &$map) {
+  function get_start_coordinates(&$map) {
+    foreach($map as $row_index => $row) {
+      foreach($row as $column_index => $symbol) {
+        if($symbol === "S") return array($row_index, $column_index);
+      }
+    }
+  }
+
+  function second_tile_coordinates($current_row, $current_column, &$map) {
     $symbol = $map[$current_row - 1][$current_column];
-    if(in_array($symbol, CONNECTIONS['SOUTH'])) return [$current_row - 1, $current_column];
+    if(in_array($symbol, CONNECTIONS['S'])) return [$current_row - 1, $current_column];
 
     $symbol = $map[$current_row + 1][$current_column];
-    if(in_array($symbol, CONNECTIONS['NORTH'])) return [$current_row + 1, $current_column];
+    if(in_array($symbol, CONNECTIONS['N'])) return [$current_row + 1, $current_column];
 
     $symbol = $map[$current_row][$current_column - 1];
-    if(in_array($symbol, CONNECTIONS['EAST'])) return [$current_row, $current_column - 1];
+    if(in_array($symbol, CONNECTIONS['E'])) return [$current_row, $current_column - 1];
 
     $symbol = $map[$current_row][$current_column + 1];
-    if(in_array($symbol, CONNECTIONS['WEST'])) return [$current_row, $current_column + 1];
+    if(in_array($symbol, CONNECTIONS['W'])) return [$current_row, $current_column + 1];
   }
 
-  function north_tile_coordinates($current_row, $current_column, &$map) {
-    $symbol = $map[$current_row][$current_column];
-
-    if(in_array($symbol, CONNECTIONS['NORTH'])) return [$current_row - 1, $current_column];
+  function north_tile_coordinates($symbol, $current_row, $current_column, &$map) {
+    if(in_array($symbol, CONNECTIONS['N'])) return [$current_row - 1, $current_column];
   }
 
-  function south_tile_coordinates($current_row, $current_column, &$map) {
-    $symbol = $map[$current_row][$current_column];
-
-    if(in_array($symbol, CONNECTIONS['SOUTH'])) return [$current_row + 1, $current_column];
+  function south_tile_coordinates($symbol, $current_row, $current_column, &$map) {
+    if(in_array($symbol, CONNECTIONS['S'])) return [$current_row + 1, $current_column];
   }
 
-  function west_tile_coordinates($current_row, $current_column, &$map) {
-    $symbol = $map[$current_row][$current_column];
-
-    if(in_array($symbol, CONNECTIONS['WEST'])) return [$current_row, $current_column - 1];
+  function west_tile_coordinates($symbol, $current_row, $current_column, &$map) {
+    if(in_array($symbol, CONNECTIONS['W'])) return [$current_row, $current_column - 1];
   }
 
-  function east_tile_coordinates($current_row, $current_column, &$map) {
-    $symbol = $map[$current_row][$current_column];
-
-    if(in_array($symbol, CONNECTIONS['EAST'])) return [$current_row, $current_column + 1];
+  function east_tile_coordinates($symbol, $current_row, $current_column, &$map) {
+    if(in_array($symbol, CONNECTIONS['E'])) return [$current_row, $current_column + 1];
   }
 
   function next_pipe_coordinates(&$path, &$map) {
@@ -85,25 +78,18 @@
 
     $row = $current_pipe_coordinates[0];
     $column = $current_pipe_coordinates[1];
-
     $symbol = $map[$row][$column];
 
-    if ($symbol === "S") {
-      $step_two_coords = step_two_tile_coordinates($row, $column, $map);
-      $path[] = $step_two_coords;
-      return $step_two_coords;
-    }
-
-    $north_coords = north_tile_coordinates($row, $column, $map);
+    $north_coords = north_tile_coordinates($symbol, $row, $column, $map);
     if(valid_path($north_coords, $path)) return $north_coords;
 
-    $south_coords = south_tile_coordinates($row, $column, $map);
+    $south_coords = south_tile_coordinates($symbol, $row, $column, $map);
     if(valid_path($south_coords, $path)) return $south_coords;
 
-    $west_coords = west_tile_coordinates($row, $column, $map);
+    $west_coords = west_tile_coordinates($symbol, $row, $column, $map);
     if(valid_path($west_coords, $path)) return $west_coords;
 
-    $east_coords = east_tile_coordinates($row, $column, $map);
+    $east_coords = east_tile_coordinates($symbol, $row, $column, $map);
     if(valid_path($east_coords, $path)) return $east_coords;
   }
 
